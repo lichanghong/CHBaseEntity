e0fb215e8dee29013ba46b7a1bd65d393ec1fc5e
f27_100008550770_yongsheng <f27_100008550770_yongsheng@git.cloud.tencent.com>
2019-06-18 06:59:52 +0800

    init

--- Example/Pods/CHBaseEntity/CHBaseEntity/Classes/CHBaseEntity.m
+++ Example/Pods/CHBaseEntity/CHBaseEntity/Classes/CHBaseEntity.m
@@ -7,7 +7,7 @@
 
 #import "CHBaseEntity.h"
 #import "YYModel.h"
-
+ 
 @interface CHBaseEntity()
 
 @end
@@ -26,7 +26,7 @@
 
 + (NSMutableArray *)arrayOfModelsWithDictionaries:(NSArray *)dictionaries
 {
-    id entity =  [self arrayOfModelsWithDictionaries:dictionaries];
+   id entity = [NSArray yy_modelArrayWithClass:self.class json:dictionaries];
     return entity;
 }
 
